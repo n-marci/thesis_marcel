48dc3a264c4e0d07d4834fa9f5bf1886185be260
n-marci <neugebauer.marcel@web.de>
2024-05-06 16:44:50 +0200

    Trying out different stuff with jupyter

--- full_model/jupyter/kernels.nix
+++ full_model/jupyter/kernels.nix
@@ -1,5 +1,12 @@
 {pkgs, ...}: {
   kernel.python.minimal = {
     enable = true;
+    # extraPackages = ps: [ ps.dulwich ];
+    # requiredRuntimePackages = [ pkgs.poetry ];
+  };
+  kernel.python.python_plus_modules = {
+    enable = true;
+    projectDir = ./python_plus_modules;
+    requiredRuntimePackages = [ pkgs.poetry ];
   };
 }
